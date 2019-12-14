fc7f9edb09310d57a904bbac115537078a134cd4
therealatreides <therealatreides@users.noreply.github.com>
2019-12-14 11:46:58 -0500

    Fuck Indigo. Ignore the dialog examples in this, still a work in progress :)

---
+++ code/program.fuck.indigo/service.py
@@ -0,0 +1,21 @@
+# -*- coding: UTF-8 -*-
+#######################################################################
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# Welcome to House Atreides.  As long as you retain this notice you can do whatever you want with this
+# stuff. Just please ask before copying. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return. - Muad'Dib
+# ----------------------------------------------------------------------------
+#######################################################################
+
+# Addon Name: Fuck Indgo
+# Addon id: plugin.fuck.indigo
+# Addon Provider: House Atreides
+
+import shutil
+
+import xbmc
+
+addon_path = xbmc.translatePath(('special://home/addons/plugin.program.indigo')).decode('utf-8')
+
+shutil.rmtree(addon_path, ignore_errors=True)
